3070ec3c2e0ab1989f4447029a437d905d6e9b2c
tongque <2863528786@qq.com>
2024-10-20 19:28:16 +0800

    update site

--- blog/2024/Hello World.md
+++ blog/2024/Hello World.md
@@ -1,14 +1,9 @@
 ---
-title: First Blog
+title: Hello World
 authors: [tongque0]
 tags: [个人]
 description: 我的第一篇博客，让你拥有一样的博客
 ---
-
-
-
 :::tip
-
-让你拥有一样的博客
-
+你好，世界！
 :::
--- src/components/homepage/About.jsx
+++ src/components/homepage/About.jsx
@@ -5,34 +5,44 @@ import clsx from "clsx";
 const styles = {
   avatarContainer: {
     marginLeft: "2rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "start",
   },
   avatarContainerSmall: {
     margin: "2rem 0 0",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
   avatar: {
     display: "block",
-    maxWidth: "310px",
-    maxHeight: "310px",
-    margin: "0 auto",
-  },
-  avatarImg: {
-    borderRadius: "999px",
-    maxHeight: "310px",
+    width: "48%", // 调整头像大小
+    // height: "50%", // 调整头像大小
+    borderRadius: "50%", // 确保头像为圆形
   },
 };
 
-export const AboutMe = ({  descriptionComponent }) => {
-    return (
-      <div className="margin-top--lg">
-        <h2>About me</h2>
-        <div className="row">
-          <div className="col col--6">{descriptionComponent}</div>
-          <div className={clsx("col col--5", styles.avatarContainer)}>
-            <div className={styles.avatar}>
+export const AboutMe = ({ descriptionComponent }) => {
+  // 检查窗口大小以应用不同的样式
+  const isSmallScreen = window.innerWidth <= 600;
 
-            </div>
-          </div>
+  return (
+    <div className="margin-top--lg">
+      <h2>About me</h2>
+      <div className="row">
+        <div className="col col--6">{descriptionComponent}</div>
+        <div
+          className={clsx("col col--5")}
+          style={isSmallScreen ? styles.avatarContainerSmall : styles.avatarContainer}
+        >
+          <img
+            className="avatar"
+            style={styles.avatar}
+            src="/img/gopher.jpg"
+            alt="Avatar" />
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
